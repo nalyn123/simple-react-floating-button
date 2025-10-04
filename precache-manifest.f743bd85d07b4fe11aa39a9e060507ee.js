9891f45de86cc416c04cacaaf7b354df7d2c0619
Rona <taguiamronalyn123@gmail.com>
2025-10-04 17:34:46 +0800

    Updates

---
+++ precache-manifest.f743bd85d07b4fe11aa39a9e060507ee.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfadb323270a74abbb5a1d189e397f3e",
+    "url": "/simple-react-floating-button/example/index.html"
+  },
+  {
+    "revision": "7623c7ad0b0a687614f4",
+    "url": "/simple-react-floating-button/example/static/css/main.144dcf78.chunk.css"
+  },
+  {
+    "revision": "1994c5ef0cd84d4cdc41",
+    "url": "/simple-react-floating-button/example/static/js/2.cd745c7e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/simple-react-floating-button/example/static/js/2.cd745c7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7623c7ad0b0a687614f4",
+    "url": "/simple-react-floating-button/example/static/js/main.b31a6e43.chunk.js"
+  },
+  {
+    "revision": "f9701dd2623a913779e1",
+    "url": "/simple-react-floating-button/example/static/js/runtime-main.eda955af.js"
+  },
+  {
+    "revision": "0e71de4717c0f9e99e570484e6039fed",
+    "url": "/simple-react-floating-button/example/static/media/basketball.0e71de47.jpg"
+  },
+  {
+    "revision": "58ea513fc15e9aa7acce0359538220fd",
+    "url": "/simple-react-floating-button/example/static/media/soccer.58ea513f.jpg"
+  }
+]);
